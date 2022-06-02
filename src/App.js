6b3dcd1390d7fb4b2e90a01c699574ab08107f38
fpatrilla <fpatrilla@gmail.com>
2022-06-01 22:17:41 -0300

    entrega

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Muebles Store</h1>
+      <h3>Federico Patrilla</h3>
+    </div>
+  );
+}
+
+export default App;
